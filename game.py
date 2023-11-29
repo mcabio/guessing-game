ce8175661f87feb8732f250b52b16f380f8cd621
mcabio <meryllou7@gmail.com>
2023-11-29 13:34:56 -0600

    Guessing Game

--- game.py
+++ game.py
@@ -1,1 +1,19 @@
-print("hello")
+user_name = input("Hi, there, what's your name?: ")
+print("Hello, " + user_name + "! Nice to meet you. Let's play a game.")
+
+import random
+
+random_num = random.randint(1,100)
+guess_num = int(input("Pick a number between 1 and 100. >>> "))
+
+while guess_num != random_num:
+    if guess_num < random_num:
+        print()
+        guess_num = int(input("Sorry, guess again...the number is higher. >>> "))
+        print()
+    elif guess_num > random_num:
+        print()
+        guess_num = int(input("Sorry, guess again...the number is lower. >>> "))
+        print()
+
+print("Congratulations, you guessed it!")
