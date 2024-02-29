c0b53327166b27d9418e6dc1df9a87b93e1bb68d
Meryl Lou Cabio <meryllou7@gmail.com>
2024-02-29 05:22:51 -0600

    Added a way to return best score out of all the games player played

--- game.py
+++ game.py
@@ -28,24 +28,35 @@ def guessing_game(guess):
     player = input("Hello, what's your name? >>> ")
     print()
     print(f"Nice to meet you, {player}, let's play a guessing game!")
+    scores = []
 
     while True:
         secret_num = random.randint(1,100)
         print()
         guess = int(input("Guess a number between 1 to 100. >>> "))
+        guesses = 0 # This counts how many guesses it takes for player to get to the correct number
+        
 
         while secret_num != guess:
             if secret_num < guess:
+                guesses += 1
                 guess = int(input("Guess again, the secret number is lower. >>> "))
             elif secret_num > guess:
+                guesses += 1
                 guess = int(input("Guess again, the secret number is higher. >>> "))
 
-        print("CONGRATULATIONS, YOU GUESSED CORRECTLY!")
+        print(f"CONGRATULATIONS, YOU GUESSED CORRECTLY! You guessed {guesses} times to get to the secret number!")
+        scores.append(guesses)
         print()
 
         replay = input("Would you like to play again? y/n? >>> ")
-        if replay.lower() != "y":
+        if replay.lower() != "y": # if replay doesn't equal "y", the loop exits
+            best_score = scores[0]
+            for i in scores[1:]:
+                if i < best_score:
+                    best_score = i
             print("Thanks for playing!")
+            print(f"You're best score is {best_score}")
             break
 
 guessing_game(50)
